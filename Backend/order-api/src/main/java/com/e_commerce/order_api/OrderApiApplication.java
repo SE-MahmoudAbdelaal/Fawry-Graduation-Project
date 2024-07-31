007af3c881830ed0abb7794ce10fcb5910cf98cb
Shaymaa <shaymaasho20683@gmail.com>
2024-07-31 19:28:59 +0300

    add order-api demo

---
+++ Backend/order-api/src/main/java/com/e_commerce/order_api/OrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.e_commerce.order_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApiApplication.class, args);
+	}
+
+}
